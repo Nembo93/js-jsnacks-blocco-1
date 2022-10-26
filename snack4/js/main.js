dd4aed082dc6bd845a2da4bdc17b91bf5c8dcc2a
nembo93 <francesco.c193@libero.it>
2022-10-26 12:32:22 +0200

    snack4

---
+++ snack4/js/main.js
@@ -0,0 +1,9 @@
+const numeriDisp = [];
+console.log(numeriDisp);
+
+for(i = 0; i < 6; i++){
+    numero = prompt(`Inserisci un numero`);
+    if (numero % 2 !== 0){
+        numeriDisp.pusch(numero);
+    }
+}
