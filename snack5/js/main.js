e2d5e1c65c40f1b7455d19d0cad0e7f2838fe656
nembo93 <francesco.c193@libero.it>
2022-10-28 10:23:18 +0200

    snack 5 creazione commit

---
+++ snack5/js/main.js
@@ -0,0 +1,16 @@
+`use strict`;
+
+const numeri = [
+    `3`,
+    `5`,
+    `6`,
+    `9`,
+    `10`,
+    `13`
+]
+
+while (i = 0; i < numeri[i]){
+    numero = numeri[i];
+    console.log[i];
+    i++;
+}
